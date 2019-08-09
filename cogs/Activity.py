0c52cd1080fec917bf6b277bba1d9711b03514fd
geni <me@geni.site>
2019-08-10 01:38:20 +0300

    thing

---
+++ cogs/Activity.py
@@ -0,0 +1,407 @@
+import json
+import os
+from discord.ext import commands
+import discord
+import asyncio
+from collections import defaultdict
+from io import BytesIO
+from pathlib import Path
+import plotly.graph_objects as go
+import datetime
+import pymysql.cursors
+import time
+import traceback
+import arrow
+import json
+
+
+
+class Activity(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.config = json.load(open(Path("config/sql.json").resolve()))
+
+        self.connect()
+        self.connection.close()  # wow
+
+    def connect(self):
+        self.connection = pymysql.connect(host=self.config["host"],
+                                          user=self.config["user"],
+                                          password=self.config["password"],
+                                          db=self.config["db"],
+                                          charset='utf8mb4',
+                                          cursorclass=pymysql.cursors.DictCursor
+                                          )
+        self.connection.autocommit(True)
+
+    def get_name(self, id: int, table: str):
+        sql = f"SELECT * FROM `discord`.`{table}` WHERE `id` = %s"
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql, (id))
+            _return = cursor.fetchone()["name"]
+            cursor.close()
+            return _return
+
+    def get_username_from_id(self, id: int):
+        return self.get_name(id, "users")
+
+    def get_channel_name_from_id(self, id: int):
+        return self.get_name(id, "channels")
+
+    def get_server_name_from_id(self, id: int):
+        return self.get_name(id, "servers")
+
+    def get_channel_ids_from_server_id(self, id: int):
+        channel_ids_in_server = []
+        sql = "SELECT * FROM `discord`.`channels` WHERE `serverid` = %s"
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql, (id))
+            for val in cursor.fetchall():
+                channel_ids_in_server.append(val["id"])
+            cursor.close()
+        return channel_ids_in_server
+
+    @commands.command()
+    @commands.is_owner()
+    async def update(self, ctx):
+        if self.connection.open:
+            return
+        try:
+            message_counts = defaultdict(int)
+            usernames = defaultdict(str)
+            self.connect()
+
+            sql = "SELECT `timestamp` FROM `discord`.`channels` WHERE `id` = %s"
+            timestamp = None
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, (ctx.message.channel.id))
+                try:
+                    timestamp = cursor.fetchone()["timestamp"]
+                except:
+                    pass
+                cursor.close()
+
+            start_timestamp = datetime.datetime.utcnow() + datetime.timedelta(microseconds=10)
+            async for msg in ctx.channel.history(limit=None, after=timestamp):
+                message_counts[msg.author.id] += 1
+                if msg.author.id not in usernames.values():
+                    usernames[msg.author.id] = msg.author.name
+
+            for key in [*message_counts]:
+                if key == 0:
+                    continue
+                with self.connection.cursor() as cursor:
+                    sql = "REPLACE INTO `discord`.`users` VALUES (%s, %s)"
+                    cursor.execute(sql, (key, usernames[key]))
+
+                    if timestamp is None:
+                        sql = "REPLACE INTO `discord`.`activity` VALUES (%s, %s, %s)"
+                        cursor.execute(
+                            sql, (key, ctx.message.channel.id, message_counts[key]))
+                    else:
+                        sql = "UPDATE `discord`.`activity` SET `messages` = `messages` + %s WHERE `userid` = %s AND `channelid` = %s"
+                        cursor.execute(
+                            sql, (message_counts[key], key, ctx.message.channel.id))
+                    cursor.close()
+
+            messages_in_channel = 0
+            sql = "SELECT * FROM `discord`.`activity` WHERE `channelid` = %s"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, (ctx.message.channel.id))
+                for val in cursor.fetchall():
+                    messages_in_channel += val["messages"]
+                cursor.close()
+
+            sql = "REPLACE INTO `discord`.`channels` (`id`, `serverid`, `name`, `timestamp`, `messages`) VALUES (%s, %s, %s, %s, %s)"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, (ctx.message.channel.id,
+                                     ctx.message.guild.id if ctx.message.guild is not None else None,
+                                     ctx.message.channel.name,
+                                     start_timestamp,
+                                     messages_in_channel))
+                cursor.close()
+
+            if ctx.message.guild is not None:
+                messages_in_server = 0
+                for channel_id in self.get_channel_ids_from_server_id(ctx.message.guild.id):
+                    sql = "SELECT * FROM `discord`.`activity` WHERE `channelid` = %s"
+                    with self.connection.cursor() as cursor:
+                        cursor.execute(sql, (channel_id))
+                        for val in cursor.fetchall():
+                            messages_in_server += val["messages"]
+                        cursor.close()
+
+                sql = "REPLACE INTO `discord`.`servers` VALUES (%s, %s, %s)"
+                with self.connection.cursor() as cursor:
+                    cursor.execute(sql, (ctx.message.guild.id,
+                                         ctx.message.guild.name,
+                                         messages_in_server))
+                    cursor.close()
+        except Exception:
+            traceback.print_exc()
+        finally:
+            self.connection.close()
+            print(f"done {ctx.message.id}")
+
+    @commands.command()
+    @commands.is_owner()
+    async def update_per_hour(self, ctx):
+        if self.connection.open:
+            return
+        try:
+            thing = defaultdict(int)
+            self.connect()
+
+            sql = "SELECT `timestamp` FROM `discord`.`channels` WHERE `id` = %s"
+            timestamp = None
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, (ctx.message.channel.id))
+                try:
+                    timestamp = cursor.fetchone()["hour_timestamp"]
+                except:
+                    pass
+                cursor.close()
+
+            async for msg in ctx.channel.history(limit=None, after=timestamp):
+                thing[msg.created_at.replace(
+                    minute=0, second=0, microsecond=0)] += 1
+
+            with self.connection.cursor() as cursor:
+                start_timestamp = datetime.datetime.utcnow() + datetime.timedelta(microseconds=10)
+                sql = "UPDATE `discord`.`channels` SET `hour_timestamp` = %s WHERE `id` = %s"
+                cursor.execute(
+                    sql, (start_timestamp, ctx.message.channel.id))
+
+            for key in [*thing]:
+                if key == 0:
+                    continue
+                with self.connection.cursor() as cursor:
+                    if timestamp is None:
+                        sql = "REPLACE INTO `discord`.`messages_per_hour` VALUES (%s, %s, %s)"
+                        cursor.execute(
+                            sql, (ctx.message.channel.id, key, thing[key]))
+                    else:
+                        sql = "UPDATE `discord`.`messages_per_hour` SET `messages` = `messages` + %s WHERE `channelid` = %s"
+                        cursor.execute(
+                            sql, (thing[key], ctx.message.channel.id))
+                    cursor.close()
+        except Exception:
+            traceback.print_exc()
+        finally:
+            self.connection.close()
+            print(f"done {ctx.message.id}")
+
+    @commands.command()
+    @commands.is_owner()
+    async def activity_per_user(self, ctx):
+        if self.connection.open:
+            return
+        try:
+            self.connect()
+            thing = defaultdict(int)
+
+            sql = "SELECT * FROM `discord`.`activity` WHERE `channelid` = %s"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, (ctx.message.channel.id))
+                for val in cursor.fetchall():
+                    thing[self.get_username_from_id(
+                        val["userid"])] = val["messages"]
+
+            fig = go.Figure(
+                data=[go.Pie(labels=list(thing.keys()), values=list(thing.values()))])
+            img_bytes = fig.to_image(format="png", width=2240, height=1600)
+            await ctx.send(file=discord.File(BytesIO(img_bytes), filename="guy.png"))
+            fig.show()
+        except Exception:
+            traceback.print_exc()
+        finally:
+            self.connection.close()
+
+    @commands.command()
+    @commands.is_owner()
+    async def activity_per_server(self, ctx):
+        if self.connection.open:
+            return
+        try:
+            self.connect()
+            thing = defaultdict(int)
+
+            sql = "SELECT * FROM `discord`.`servers`"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, ())
+                for val in cursor.fetchall():
+                    thing[val["name"]] = val["messages"]
+
+            sql = "SELECT * FROM `discord`.`channels` WHERE `serverid` IS NULL"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, ())
+                for val in cursor.fetchall():
+                    thing[val["name"]] = val["messages"]
+
+            fig = go.Figure(
+                data=[go.Bar(x=list(thing.keys()), y=list(thing.values()))])
+            img_bytes = fig.to_image(format="png", width=2240, height=1600)
+            await ctx.send(file=discord.File(BytesIO(img_bytes), filename="guy.png"))
+            fig.show()
+        except Exception:
+            traceback.print_exc()
+        finally:
+            self.connection.close()
+
+    @commands.command()
+    @commands.is_owner()
+    async def uc(self, ctx):
+        if self.connection.open:
+            return
+        try:
+            self.connect()
+            guy = []
+            thing = defaultdict(int)
+
+            sql = "SELECT * FROM `discord`.`users`"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, ())
+                for val in cursor.fetchall():
+                    guy.append(val["id"])
+                cursor.close()
+
+            for user in guy:
+                sql = "SELECT * FROM `discord`.`activity` WHERE `userid` = %s"
+                with self.connection.cursor() as cursor:
+                    cursor.execute(sql, (user))
+                    for val in cursor.fetchall():
+                        thing[self.get_username_from_id(
+                            user)] += val["messages"]
+                    cursor.close()
+
+            fig = go.Figure(
+                data=[go.Pie(labels=list(thing.keys()), values=list(thing.values()))])
+            img_bytes = fig.to_image(format="png", width=2240, height=1600)
+            await ctx.send(file=discord.File(BytesIO(img_bytes), filename="guy.png"))
+            fig.show()
+        except Exception:
+            traceback.print_exc()
+        finally:
+            self.connection.close()
+
+    @commands.command()
+    @commands.is_owner()
+    async def user_messages_per_server(self, ctx, member: discord.User = None):
+        if self.connection.open:
+            return
+        if member is None:
+            member = ctx.author
+        try:
+            self.connect()
+            thing = defaultdict(int)
+
+            sql = "SELECT * FROM `discord`.`activity` WHERE `userid` = %s AND `serverid` IS NULL"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, (member.id))
+                for val in cursor.fetchall():
+                    thing[self.get_channel_name_from_id(
+                        val["channelid"])] += val["messages"]
+                cursor.close()
+
+            sql = "SELECT * FROM `discord`.`activity` WHERE `userid` = %s AND `serverid` IS NOT NULL"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, (member.id))
+                for val in cursor.fetchall():
+                    thing[self.get_server_name_from_id(
+                        val["serverid"])] += val["messages"]
+                cursor.close()
+
+            fig = go.Figure(
+                data=[go.Pie(labels=list(thing.keys()), values=list(thing.values()))])
+            img_bytes = fig.to_image(format="png", width=2240, height=1600)
+            await ctx.send(file=discord.File(BytesIO(img_bytes), filename="guy.png"))
+            fig.show()
+        except Exception:
+            traceback.print_exc()
+        finally:
+            self.connection.close()
+
+    @commands.command()
+    @commands.is_owner()
+    async def activity_per_hour(self, ctx):
+        if self.connection.open:
+            return
+        try:
+            self.connect()
+            thing = defaultdict(int)
+
+            sql = "SELECT * FROM `discord`.`messages_per_hour` WHERE `channelid` = %s"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, (ctx.message.channel.id))
+                for val in cursor.fetchall():
+                    thing[str(val["timestamp"])] = val["messages"]
+                cursor.close()
+
+            fig = go.Figure(
+                data=[go.Scatter(x=list(thing.keys()), y=list(thing.values()))])
+            img_bytes = fig.to_image(format="png", width=2240, height=1600)
+            await ctx.send(file=discord.File(BytesIO(img_bytes), filename="guy.png"))
+            fig.show()
+        except Exception:
+            traceback.print_exc()
+        finally:
+            self.connection.close()
+
+    @commands.command()
+    @commands.is_owner()
+    async def activity_per_day(self, ctx):
+        if self.connection.open:
+            return
+        try:
+            self.connect()
+            thing = defaultdict(int)
+
+            sql = "SELECT * FROM `discord`.`messages_per_hour` WHERE `channelid` = %s"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, (ctx.message.channel.id))
+                for val in cursor.fetchall():
+                    thing[str(val["timestamp"].replace(hour=0))
+                          ] += val["messages"]
+                cursor.close()
+
+            fig = go.Figure(
+                data=[go.Scatter(x=list(thing.keys()), y=list(thing.values()))])
+            img_bytes = fig.to_image(format="png", width=2240, height=1600)
+            await ctx.send(file=discord.File(BytesIO(img_bytes), filename="guy.png"))
+            fig.show()
+        except Exception:
+            traceback.print_exc()
+        finally:
+            self.connection.close()
+
+    @commands.command()
+    @commands.is_owner()
+    async def activity_per_month(self, ctx):
+        if self.connection.open:
+            return
+        try:
+            self.connect()
+            thing = defaultdict(int)
+
+            sql = "SELECT * FROM `discord`.`messages_per_hour` WHERE `channelid` = %s"
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql, (ctx.message.channel.id))
+                for val in cursor.fetchall():
+                    thing[str(val["timestamp"].replace(
+                        day=1, hour=0))] += val["messages"]
+                cursor.close()
+
+            fig = go.Figure(
+                data=[go.Scatter(x=list(thing.keys()), y=list(thing.values()))])
+            img_bytes = fig.to_image(format="png", width=2240, height=1600)
+            await ctx.send(file=discord.File(BytesIO(img_bytes), filename="guy.png"))
+            fig.show()
+        except Exception:
+            traceback.print_exc()
+        finally:
+            self.connection.close()
+
+
+def setup(bot):
+    bot.add_cog(Activity(bot))
---
+++ taiga.py
@@ -0,0 +1,171 @@
+import json
+
+import os
+import logging
+import datetime
+from pathlib import Path
+
+import discord
+from discord import Forbidden, ConnectionClosed
+from discord.ext import commands
+from discord.opus import OpusError, OpusNotLoaded
+from discord.ext.commands import CommandOnCooldown, CommandNotFound, NotOwner
+
+# pylint: disable=W0703
+# pylint: disable=W1202
+
+def taiga():
+    """the main thing"""
+    cogs_json = Path("config/cogs.json").resolve()  # cog list
+    config = Path("config/config.json").resolve()  # config file
+
+    with open(config) as file:
+        data = json.load(file)
+    prefix = data['prefix']
+    description = data['description']
+    token = data['token']
+    # making sure it's an int even if someone makes it a string
+    owner = int(data['owner_id'])
+
+    version = data['version']
+    log_name = data['log']
+    pm_help = data['pm_help']
+
+    taiga_log = logging.getLogger("discord")
+    taiga_log.setLevel(logging.INFO)
+    handler = logging.FileHandler(
+        filename="logs/{}".format(log_name), encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    taiga_log.addHandler(handler)
+
+    bot = commands.Bot(
+        command_prefix=prefix,
+        description=description,
+        owner_id=owner)
+
+    if pm_help.lower() == 'true':  # pm help checks
+        bot.pm_help = True
+
+    @bot.event
+    async def on_ready():
+        """does stuff when the bot is ready"""  # pretty much
+        servers = len(bot.guilds)
+        activity = discord.Game(name=data['game'].format(
+            bot.command_prefix, str(servers)))
+        await bot.change_presence(activity=activity)
+        print("Username: {}".format(bot.user.name))
+        print("ID: {}".format(str(bot.user.id)))
+        print("Prefix: {}".format(bot.command_prefix))
+        print("Version: {}".format(version))
+        print("Game: {}".format(activity))
+        print("Description: {}".format(description))
+        print("Owner ID: {}".format(str(owner)))
+
+    async def prlog(msg, etype):
+        print(msg)
+        if etype == "error":
+            taiga_log.error(msg)
+        elif etype == "warning":
+            taiga_log.warning(msg)
+        elif etype == "info":
+            taiga_log.info(msg)
+
+    @bot.event
+    async def on_command_error(ctx, error):
+        channel = ctx.message.channel
+        if isinstance(error, CommandOnCooldown):
+            await channel.send("the command {.command} is on cooldown, {}. wait {:.2f} seconds to use it again.".format(ctx,
+                                                                                                                        ctx.message.author.name,
+                                                                                                                        error.retry_after))
+        elif isinstance(error, CommandNotFound):
+            await channel.send("command not found")
+        elif isinstance(error, NotOwner):
+            await channel.send("fuck off")
+        elif isinstance(error, OpusError):
+            await prlog("libopus error: {}".format(error), "error")
+        elif isinstance(error, OpusNotLoaded):
+            await prlog("libopus isn't loaded: {}".format(error), "error")
+        elif isinstance(error, ConnectionClosed):
+            await prlog("connection closed? possibly internet connection issue. reason: {}\ncode: {}\n shard ID: {}".format(error.reason, error.code, error.shard_id), "error")
+        elif isinstance(error, Forbidden):
+            await channel.send("nope")
+
+    with open(cogs_json) as cogs_list:
+        cogs = json.load(cogs_list)
+
+    for value in cogs:
+        try:
+            bot.load_extension(value)
+            msg = "Loaded extension {}.".format(value)
+            print(msg)
+            taiga_log.info(msg)
+        except ImportError as thing:
+            exc = "{}: {}".format(
+                type(thing).__name__, thing)
+            msg = "Failed to load extension {}\n{}".format(value, exc)
+            print(msg)
+            taiga_log.error(msg)
+        except discord.ClientException as thing:
+            msg = "{} doesn't have a setup function.".format(value)
+            print(msg)
+            taiga_log.error(msg)
+        except Exception as thing:
+            exc = "{}: {}".format(
+                type(thing).__name__, thing)
+            msg = "Failed to load extension {}\n{}".format(value, exc)
+            print(msg)
+            taiga_log.error(msg)
+
+    @bot.command(hidden=True)
+    @commands.is_owner()
+    async def load_cog(ctx, *, cog: str):
+        """Loads a cog."""
+        try:
+            bot.load_extension(cog)
+        except Exception as thing:
+            exc = "{}: {}".format(type(thing).__name__, thing)
+            msg = "```Failed to load extension {}\n{}```".format(cog, exc)
+            await ctx.send(msg)
+
+    @bot.command(hidden=True)
+    @commands.is_owner()
+    async def unload_cog(ctx, *, cog: str):
+        """Unloads a cog."""
+        try:
+            bot.unload_extension(cog)
+        except Exception as thing:
+            exc = "{}: {}".format(type(thing).__name__, thing)
+            msg = "```Failed to unload extension {}\n{}```".format(cog, exc)
+            await ctx.send(msg)
+
+    @bot.command(hidden=True)
+    @commands.is_owner()
+    async def reload_cog(ctx, *, cog: str):
+        """Reloads a cog."""
+        try:
+            bot.unload_extension(cog)
+            bot.load_extension(cog)
+        except Exception as thing:
+            exc = "{}: {}".format(type(thing).__name__, thing)
+            msg = "```Failed to reload extension {}\n{}```".format(cog, exc)
+            await ctx.send(msg)
+
+    @bot.command(hidden=True)
+    @commands.is_owner()
+    async def reload_all(ctx):
+        """reloads all cogs"""
+        for cog in cogs:
+            try:
+                bot.unload_extension(cog)
+                bot.load_extension(cog)
+            except Exception as thing:
+                exc = "{}: {}".format(type(thing).__name__, thing)
+                msg = "```Failed to reload extension {}\n{}```".format(cog, exc)
+                await ctx.send(msg)
+
+    bot.run(token, bot=False)
+
+
+if __name__ == "__main__":
+    taiga()
